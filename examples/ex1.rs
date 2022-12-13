58125b977eaf0e5891686d754fdcffd6f00ecacd
Seimizu Joukan <joukan.seimizu@gmail.com>
2022-12-13 13:46:37 +0900

    Update example by using tracing instead of log.

--- examples/ex1.rs
+++ examples/ex1.rs
@@ -1,19 +1,67 @@
+use std::fmt::Display;
+
 use clap::Parser;
 use function_name::named;
 #[allow(unused)]
-use jlogger_tracing::{
-    jdebug, jerror, jinfo, jtrace, jwarn, JloggerBuilder, LevelFilter, LogTimeFormat,
+use {
+    jlogger_tracing::{
+        jdebug, jerror, jinfo, jtrace, jwarn, JloggerBuilder, LevelFilter, LogTimeFormat,
+    },
+    tracing::{debug, error, info, span, trace, warn},
 };
+
+#[derive(Debug, Clone, Copy)]
+enum TimeFormat {
+    None,
+    Local,
+    TimeStamp,
+}
+
+impl Display for TimeFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            TimeFormat::None => "none",
+            TimeFormat::Local => "local",
+            TimeFormat::TimeStamp => "timestamp",
+        };
+
+        write!(f, "{}", s)
+    }
+}
+
+impl From<&str> for TimeFormat {
+    fn from(s: &str) -> Self {
+        match s {
+            "local" => TimeFormat::Local,
+            "timestamp" => TimeFormat::TimeStamp,
+            _ => TimeFormat::None,
+        }
+    }
+}
+
+impl From<TimeFormat> for LogTimeFormat {
+    fn from(t: TimeFormat) -> Self {
+        match t {
+            TimeFormat::None => LogTimeFormat::TimeNone,
+            TimeFormat::Local => LogTimeFormat::TimeLocal,
+            TimeFormat::TimeStamp => LogTimeFormat::TimeStamp,
+        }
+    }
+}
 
 #[derive(Parser, Debug)]
 /// Jlogger example program.
 struct Cli {
     #[clap(short, long)]
     log_file: Option<String>,
+
+    #[clap(short, long, default_value_t = TimeFormat::None)]
+    time_format: TimeFormat,
 }
 
 #[named]
 pub fn level1() {
+    let _level1_span = span!(tracing::Level::INFO, "level1_span").entered();
     jinfo!("{}", function_name!());
 }
 
@@ -25,7 +73,9 @@ pub fn level2() {
 
 #[named]
 pub fn level3() {
-    jinfo!("{}", function_name!());
+    let _level3_span = span!(tracing::Level::INFO, "level3_span").entered();
+
+    info!(f_name = function_name!(), line = line!(), msg = "hello");
     level2();
 }
 
@@ -39,9 +89,9 @@ pub fn main() {
         .log_file(cli.log_file.as_ref().map(|a| (a.as_str(), false)))
         .log_console(log_console)
         .log_runtime(true)
-        .log_time(LogTimeFormat::TimeStamp)
+        .log_time(LogTimeFormat::from(cli.time_format))
         .build();
 
-    log::info!("{}", function_name!());
+    info!(f_name = function_name!(), line = line!(), msg = "hello");
     level3();
 }
